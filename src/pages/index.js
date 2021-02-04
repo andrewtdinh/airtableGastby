b7ac943cf3c604040529b2e6cbc0070413457313
Andrew Dinh <andrewd@benchling.com>
2021-02-04 09:21:32 -0800

    Get the airtable field Name to show up as a list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
-import React from "react"
+import React from "react";
+import { graphql } from 'gatsby';
 
-export default function Home() {
-  return <div>Hello world!</div>
+export const query = graphql`
+  {
+    allAirtable(filter: {table: {eq: "Items"}}) {
+      nodes {
+        recordId
+        data {
+          Name
+        }
+      }
+    }
+  }
+`;
+
+export default function Home({ data }) {
+  return (
+    <div>
+      <h2>My Resume</h2>
+      <ul>
+        {data.allAirtable.nodes.map(node => (
+          <li key={node.recordId}>{node.data.Name}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
